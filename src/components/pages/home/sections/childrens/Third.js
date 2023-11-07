7939aeb03853e4b2b9ab9aa9a78e54adb4c0a6a4
Enes Gökkaya <eneagkky@gmail.com>
2023-11-07 15:46:34 +0300

    'v1.0'

--- src/components/pages/home/sections/childrens/Third.js
+++ src/components/pages/home/sections/childrens/Third.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Link from './components/Link'
-import { AiFillCode, AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillMail, AiFillMediumCircle, AiFillTwitterCircle, AiFillYoutube } from 'react-icons/ai'
+import { AiFillCode, AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillMail, AiFillShopping, AiFillMediumCircle, AiFillTwitterCircle, AiFillYoutube } from 'react-icons/ai'
 import { BsPinterest } from 'react-icons/bs'
+import { TbBrandThreads } from 'react-icons/tb'
 
 const Third = () => {
     return (
@@ -9,23 +10,25 @@ const Third = () => {
             className='w-full h-full flex justify-center items-center text-white'
         >
             <div
-                className='border p-4 flex-col test'
+                className='border py-4 flex-col test'
             >
                 <h1
-                    className='text-center font-mono text-2xl'
+                    className='text-center font-mono text-sm md:text-2xl'
                 >
                     LINKS
                 </h1>
-                <div className='px-5 flex flex-wrap gap-3 pt-3'>
-                    <Link text={'instagram'} icon={<AiFillInstagram color='pink'/>} color='pink' url={'https://www.instagram.com/enesgkky/'}/>
-                    <Link text={'linkedin'} icon={<AiFillLinkedin color='lightblue'/>} color='pink' url={'https://www.linkedin.com/in/enes-g%C3%B6kkay'}/>
-                    <Link text={'twitter or x'} icon={<AiFillTwitterCircle color='black'/>} color='pink' url={'https://twitter.com/enesgkky'}/>
-                    <Link text={'pinteres'} icon={<BsPinterest color='red'/>} color='pink' url={'https://tr.pinterest.com/enesgkky/'}/>
-                    <Link text={'github'} icon={<AiFillGithub color='orange'/>} color='pink' url={'https://github.com/enesgkky/'}/>
-                    <Link text={'medium'} icon={<AiFillMediumCircle color='white'/>} color='pink' url={'https://medium.com/@eneagkky'}/>
-                    <Link text={'youtube'} icon={<AiFillYoutube color='red'/>} color='pink' url={'https://youtube.com/@Dev_EnesGK?si=RVCmWy732G4EHNl2'}/>
-                    <Link text={'e-mail'} icon={<AiFillMail color='white'/>} color='pink' url={'mailto:contact.enesgokkaya@gmail.com/'}/>
-                    <Link text={'codewars'} icon={<AiFillCode color='orange'/>} color='pink' url={'https://www.codewars.com/users/enesgkky/'}/>
+                <div className='px-5 flex flex-wrap text-center gap-3 pt-3'>
+                    <Link text={'instagram'} icon={<AiFillInstagram color='pink' />} color='pink' url={'https://www.instagram.com/enesgkky/'} />
+                    <Link text={'linkedin'} icon={<AiFillLinkedin color='lightblue' />} color='pink' url={'https://www.linkedin.com/in/enes-g%C3%B6kkay'} />
+                    <Link text={'twitter or x'} icon={<AiFillTwitterCircle color='black' />} color='pink' url={'https://twitter.com/enesgkky'} />
+                    <Link text={'pinteres'} icon={<BsPinterest color='red' />} color='pink' url={'https://tr.pinterest.com/enesgkky/'} />
+                    <Link text={'github'} icon={<AiFillGithub color='orange' />} color='pink' url={'https://github.com/enesgkky/'} />
+                    <Link text={'medium'} icon={<AiFillMediumCircle color='white' />} color='pink' url={'https://medium.com/@eneagkky'} />
+                    <Link text={'youtube'} icon={<AiFillYoutube color='red' />} color='pink' url={'https://youtube.com/@Dev_EnesGK?si=RVCmWy732G4EHNl2'} />
+                    <Link text={'e-mail'} icon={<AiFillMail color='white' />} color='pink' url={'mailto:contact.enesgokkaya@gmail.com/'} />
+                    <Link text={'codewars'} icon={<AiFillCode color='orange' />} color='pink' url={'https://www.codewars.com/users/enesgkky/'} />
+                    <Link text={'threads'} icon={<TbBrandThreads color='white' />} color='pink' url={'https://www.threads.net/@enesgkky'} />
+                    <Link text={'bionluk'} icon={<AiFillShopping color='pink' />} color='pink' url={'https://bionluk.com/enesgkky'} />
                 </div>
             </div>
         </article>
--- src/components/pages/home/sections/childrens/components/Link.js
+++ src/components/pages/home/sections/childrens/components/Link.js
@@ -6,9 +6,9 @@ const Link = ({ icon, text, url }) => {
             target='_blank'
             href={url}
             className={``}>
-            <div className={`flex gap-1 p-2  cursor-pointer border rounded-xl opacity-50 hover:opacity-100 transition-opacity`}>
+            <div className={`flex gap-1 items-center p-2 flex-grow-1 basis-52 cursor-pointer border rounded-xl opacity-50 hover:opacity-100 transition-opacity`}>
                 {icon}
-                <h1 className='uppercase font-mono text-xs'>{text}</h1>
+                <h1 className='uppercase font-mono text-[.6rem] md:text-xs'>{text}</h1>
             </div>
         </a>
     )
